a067b8636e4db79c48e6453f0a538231f955d530
Arve Hjønnevåg <arve@android.com>
2019-05-21 12:14:59 -0700

    [app][memorytest] Add concurrent allocation test
    Bug: 132652075
    Change-Id: Id2da5e527c441af1d281a87be92f360211645a69

--- app/memorytest/memorytest.c
+++ app/memorytest/memorytest.c
@@ -1,6 +1,9 @@
 #include <malloc.h>
 
+#include <err.h>
+#include <kernel/thread.h>
 #include <lib/unittest/unittest.h>
+#include <stdatomic.h>
 
 static uintptr_t expected_malloc_alignment(size) {
     /* TODO use ffs? */
@@ -32,6 +35,79 @@ TEST(memorytest, malloc_alignment) {
         free(ptr3);
         free(ptr2);
         free(ptr1);
+    }
+test_abort:;
+}
+
+struct memorytest_alloc_thread_arg {
+    size_t alloc_size;
+    int count;
+    atomic_bool go;
+};
+
+int memorytest_alloc_thread(void* _arg) {
+    struct memorytest_alloc_thread_arg* arg = _arg;
+    void** ptrs;
+    int i;
+    int ret;
+
+    ptrs = calloc(arg->count, sizeof(*ptrs));
+    if (!ptrs) {
+        return ERR_NO_MEMORY;
+    }
+
+    /* Busy-wait until control thread says go. */
+    while (!atomic_load(&arg->go)) {
+    }
+
+    for (i = 0; i < arg->count; i++) {
+        ptrs[i] = malloc(arg->alloc_size);
+        if (!ptrs[i]) {
+            ret = ERR_NO_MEMORY;
+            goto err_malloc;
+        }
+    }
+    ret = 0;
+
+err_malloc:
+    while (i-- > 0) {
+        free(ptrs[i]);
+    }
+    free(ptrs);
+    return ret;
+}
+
+TEST(memorytest, concurrent_allocation) {
+    /*
+     * Test concurrent allocation by creating many threads. If this test is
+     * as the first test after boot, it will test the behavior while growing
+     * the heap. Assuming the heap implemention never shrinks the heap,
+     * additional test runs will not exercise this path.
+     */
+    struct memorytest_alloc_thread_arg thread_arg = {
+            .alloc_size = PAGE_SIZE / 4 * 3, /* ~1 page after heap overhead */
+            .count = 8,
+            .go = false,
+    };
+    struct thread* thread[128];
+    for (size_t i = 0; i < countof(thread); i++) {
+        thread[i] = thread_create("memorytest", memorytest_alloc_thread,
+                                  &thread_arg, HIGH_PRIORITY - 1,
+                                  DEFAULT_STACK_SIZE);
+        ASSERT_NE(0, thread[i]);
+    }
+    for (size_t i = 0; i < countof(thread); i++) {
+        ASSERT_EQ(0, thread_resume(thread[i]));
+    }
+
+    /* Wait for test threads to start and migrate to other cpus */
+    thread_sleep(100);
+    atomic_store(&thread_arg.go, true);
+
+    for (size_t i = 0; i < countof(thread); i++) {
+        int retcode;
+        ASSERT_EQ(0, thread_join(thread[i], &retcode, INFINITE_TIME));
+        EXPECT_EQ(0, retcode);
     }
 test_abort:;
 }
