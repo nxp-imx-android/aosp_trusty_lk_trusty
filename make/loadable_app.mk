10536851061539092a57862589394950efd7b006
Andrei Homescu <ah@immunant.com>
2021-02-15 22:56:26 -0800

    [make] Sign apploader packages with ECDSA
    This CL enables application signing in the apploader.
    
    Bug: 115420908
    Change-Id: If18ab52635be9625b5d3665cf9d46f5da6ea1994

--- make/loadable_app.mk
+++ make/loadable_app.mk
@@ -26,6 +26,10 @@
 #     APP_NAME - an output file name (without extension)
 #     APP_TOP_MODULE - top module to compile
 #     APP_BUILDDIR - build directory
+#
+# To sign the app with a different key than the default one from
+# APPLOADER_SIGN_KEY_ID, set the following variable in rules.mk:
+#     APPLOADER_SIGN_KEY_ID_FOR_$(MODULE)
 
 # Build a loadable application
 LOADABLE_APP_TOOL := $(BUILDDIR)/host_tools/apploader_package_tool
@@ -33,13 +37,48 @@ LOADABLE_APP_TOOL := $(BUILDDIR)/host_tools/apploader_package_tool
 APP_ELF := $(APP_BUILDDIR)/$(APP_NAME).elf
 APP_MANIFEST := $(APP_BUILDDIR)/$(APP_NAME).manifest
 
+UNSIGNED_APP := $(APP_BUILDDIR)/$(APP_NAME).app.unsigned
 LOADABLE_APP := $(APP_BUILDDIR)/$(APP_NAME).app
 
-$(LOADABLE_APP): LOADABLE_APP_TOOL := $(LOADABLE_APP_TOOL)
-$(LOADABLE_APP): $(APP_ELF) $(APP_MANIFEST) $(LOADABLE_APP_TOOL)
+$(UNSIGNED_APP): LOADABLE_APP_TOOL := $(LOADABLE_APP_TOOL)
+$(UNSIGNED_APP): $(APP_ELF) $(APP_MANIFEST) $(LOADABLE_APP_TOOL)
 	@$(MKDIR)
 	@echo building $@ from $<
 	$(NOECHO)$(LOADABLE_APP_TOOL) -m build $@ $< $(word 2,$^)
+
+# If we have an app-specific key identifier then use it,
+# otherwise use the global default
+ifneq ($(APPLOADER_SIGN_KEY_ID_FOR_$(APP_TOP_MODULE)),)
+APP_SIGN_KEY_ID := $(APPLOADER_SIGN_KEY_ID_FOR_$(APP_TOP_MODULE))
+else
+APP_SIGN_KEY_ID := $(APPLOADER_SIGN_KEY_ID)
+endif
+
+ifneq ($(APP_SIGN_KEY_ID),)
+APP_SIGN_KEY_FILE := $(APPLOADER_SIGN_PRIVATE_KEY_$(APP_SIGN_KEY_ID)_FILE)
+endif
+
+ifneq ($(APP_SIGN_KEY_FILE),)
+$(LOADABLE_APP): LOADABLE_APP_TOOL := $(LOADABLE_APP_TOOL)
+$(LOADABLE_APP): APP_SIGN_KEY_FILE := $(APP_SIGN_KEY_FILE)
+$(LOADABLE_APP): APP_SIGN_KEY_ID := $(APP_SIGN_KEY_ID)
+$(LOADABLE_APP): $(UNSIGNED_APP) $(APP_SIGN_KEY_FILE) $(LOADABLE_APP_TOOL)
+	@$(MKDIR)
+	@echo building $@ from $<
+	$(NOECHO)$(LOADABLE_APP_TOOL) -m sign $@ $< \
+		$(APP_SIGN_KEY_FILE) $(APP_SIGN_KEY_ID)
+else
+# If we don't have a signature file, just use the unsigned file as the output
+# This is needed because modules that import loadable apps, e.g.,
+# app-mgmt-test, need the app files to exist
+# Note: apploader will refuse to load the unsigned application
+$(LOADABLE_APP): $(UNSIGNED_APP)
+	@$(MKDIR)
+	@echo copying $< to $@
+	@cp $< $@
+
+$(warning Loadable application is not signed: $(LOADABLE_APP))
+endif
 
 GENERATED += $(LOADABLE_APP)
 EXTRA_BUILDDEPS += $(LOADABLE_APP)
@@ -52,4 +91,9 @@ APP_TOP_MODULE :=
 LOADABLE_APP_TOOL :=
 APP_ELF :=
 APP_MANIFEST :=
+
+UNSIGNED_APP :=
 LOADABLE_APP :=
+
+APP_SIGN_KEY_ID :=
+APP_SIGN_KEY_FILE :=
