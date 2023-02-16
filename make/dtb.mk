ebd25e93029fb443391d7ece3e0b630685f8da90
Andrei Homescu <ahomescu@google.com>
2023-02-16 06:17:39 +0000

    make/dtb.mk: Support multiple input files per rules.mk
    Add support for multiple input files in the MODULE_DTS
    variable in the same rules.mk file, with two changes:
    * Switch to Make static pattern rules
    * Compute the INCBIN symbol name with a shell variable
      in the recipe that auto-generated the C file
    
    Bug: 269629885
    Change-Id: Ie04bcb5f77189fdaa775203117fd4a33d4d690e5

--- make/dtb.mk
+++ make/dtb.mk
@@ -32,25 +32,18 @@
 # MODULE_DTS : a .dts file (required)
 # MODULE_DTC_FLAGS : flags passed to dtc invocation
 # MODULE_DTS_INCLUDES : list of include directories used to preprocess the .dts
-# MODULE_DT_SYM : an optional symbol name for the embedded dtb
 
 MODULE_DTS_INCLUDES := $(foreach inc,$(MODULE_DTS_INCLUDES),$(addprefix -I,$(inc)))
 
 MODULE_CPP_DTS := $(patsubst %.dts,$(BUILDDIR)/%.cpp.dts,$(MODULE_DTS))
 MODULE_DTB := $(patsubst %.cpp.dts,%.dtb,$(MODULE_CPP_DTS))
-MODULE_INCBIN_C := $(patsubst %.dtb,%.c,$(MODULE_DTB))
-
-ifeq ($(MODULE_DT_SYM),)
-# Generate a symbol name by taking the module name and replacing
-# all non-alphanumeric characters in it with underscores
-MODULE_DT_SYM := $(shell printf "$(basename $(MODULE_DTS))" | tr -c '[:alnum:]' '_')
-endif
+MODULE_DTB_C := $(patsubst %.dtb,%.dtb.c,$(MODULE_DTB))
 
 DTC_PREBUILT := prebuilts/misc/linux-x86/dtc/dtc
 
 # Preprocess the .dts files
 $(MODULE_CPP_DTS): MODULE_DTS_INCLUDES := $(MODULE_DTS_INCLUDES)
-$(MODULE_CPP_DTS): $(MODULE_DTS)
+$(MODULE_CPP_DTS): $(BUILDDIR)/%.cpp.dts: %.dts
 	@$(MKDIR)
 	$(NOECHO)$(CC) -E -nostdinc $(MODULE_DTS_INCLUDES) -undef -D__DTS__ \
 		-x assembler-with-cpp -o $@ $<
@@ -58,23 +51,27 @@ $(MODULE_CPP_DTS): $(MODULE_DTS)
 # Compile each .dts into a .dtb
 $(MODULE_DTB): MODULE_DTC_FLAGS := $(MODULE_DTC_FLAGS)
 $(MODULE_DTB): DTC_PREBUILT := $(DTC_PREBUILT)
-$(MODULE_DTB): $(MODULE_CPP_DTS)
+$(MODULE_DTB): %.dtb: %.cpp.dts
 	@$(MKDIR)
 	$(NOECHO)$(DTC_PREBUILT) -O dtb -o $@ $(MODULE_DTC_FLAGS) --symbols $<
 
-# Generate the .c that embeds the .dtb in the module
-$(MODULE_INCBIN_C): MODULE_DTS := $(MODULE_DTS)
-$(MODULE_INCBIN_C): MODULE_DT_SYM := $(MODULE_DT_SYM)
-$(MODULE_INCBIN_C): MODULE_DTB := $(MODULE_DTB)
-$(MODULE_INCBIN_C): $(MODULE_DTB)
+# We generate a symbol name by taking the module name and replacing
+# all non-alphanumeric characters in it with underscores.
+# We need to do this separately for every input file since the symbol
+# names need to all be different. The command needs to be executed
+# as a shell command inside the recipe.
+DT_SYM_MANGLE_CMD = `printf "dtb_sym_$(basename $<)" | tr -c '[:alnum:]' '_'`
+
+# Generate the .c that embeds the .dtb in the module.
+$(MODULE_DTB_C): %.dtb.c: %.dtb
 	@$(MKDIR)
 	$(NOECHO) printf "#include <lk/compiler.h>\n" > $@
-	$(NOECHO) printf "INCBIN_ALIGNED(dtb_$(MODULE_DT_SYM), \
-		dtb_$(MODULE_DT_SYM)_size, \"$(MODULE_DTB)\", \".dtb\", 8);\n" >> $@
+	$(NOECHO) printf "INCBIN_ALIGNED($(DT_SYM_MANGLE_CMD), \
+		$(DT_SYM_MANGLE_CMD)_size, \"$<\", \".dtb\", 8);\n" >> $@
 
 # Add the generated .c to the module sources
 MODULE_SRCS += \
-	$(MODULE_INCBIN_C) \
+	$(MODULE_DTB_C) \
 
 include make/library.mk
 
@@ -84,8 +81,6 @@ MODULE_DTS_INCLUDES :=
 
 MODULE_CPP_DTS :=
 MODULE_DTB :=
-MODULE_INCBIN_C :=
-
-MODULE_DT_SYM :=
+MODULE_DTB_C :=
 
 DTC_PREBUILT :=
