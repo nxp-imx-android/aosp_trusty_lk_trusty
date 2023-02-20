d0fbec3f8c7a124a3672a23da6d29929d8ee2cc0
Mike McTernan <mikemcternan@google.com>
2023-02-20 22:20:59 +0000

    kernel:btitest: Only build if kernel BTI enabled on a supported arch
    Bug: 260397427
    Test: build.py qemu-generic-arm64-test-debug
    Test: build.py qemu-generic-arm32-test-debug
    Change-Id: I6b0042fe018b02bb1f12ad6bac2247b74b44e0e0

--- app/btitest/rules.mk
+++ app/btitest/rules.mk
@@ -5,14 +5,14 @@ MODULE := $(LOCAL_DIR)
 MODULE_DEPS += \
 	trusty/kernel/lib/unittest \
 
-# Only build the test module if BTI is configured for some part of the system
-ifeq (falsefalse,$(call TOBOOL,$(KERNEL_BTI_ENABLED))$(call TOBOOL,$(USER_BTI_ENABLED)))
+# Only build the test module if BTI is configured on a supported system
+ifneq (true-arm64,$(call TOBOOL,$(KERNEL_BTI_ENABLED))-$(ARCH))
 MODULE_SRCS += \
 	$(LOCAL_DIR)/btitest_stub.c
 else
 MODULE_SRCS += \
 	$(LOCAL_DIR)/btitest.c \
-	$(LOCAL_DIR)/btitest_arm64.S
+	$(LOCAL_DIR)/btitest_$(ARCH).S
 endif
 
 include make/module.mk
